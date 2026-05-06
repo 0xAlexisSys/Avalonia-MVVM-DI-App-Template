f3451e0154b58ba42ee0eec15813de2a7a5e42bc
Alexis Brooks <91137432+0xAlexisSys@users.noreply.github.com>
2026-05-07 01:58:55 +0600

    Remove random print

--- src/AvaloniaApplication/ViewModels/Windows/AcceptDialogViewModel.cs
+++ src/AvaloniaApplication/ViewModels/Windows/AcceptDialogViewModel.cs
@@ -8,8 +8,6 @@ public sealed partial class AcceptDialogViewModel : DialogViewModel
 {
     [ObservableProperty] public partial string AcceptButtonText { get; set; } = "OK";
 
-    public AcceptDialogViewModel() => Console.WriteLine("Ready");
-
     [RelayCommand]
     private static void Accept(Window window) => window.Close();
 }
